02eeb441bb9ebcb311f81840de1857e026cec8ee
Bryani8 <bryanmorochoi8@outlook.es>
2026-09-16 22:48:52 -0500

    Encapsulation example

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(5);
+        Fraction fraction2 = new Fraction(3, 4);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        fraction2.SetTop(1);
+        fraction2.SetBottom(3);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
     }
 }
